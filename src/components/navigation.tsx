4e55e319a6f69b5bb969f34796391eb17c89c88d
Anna Maier <anna.maier@codecentric.de>
2024-03-18 21:40:44 +0100

    1. Keyboard Navigation: Match DOM order and visual order

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -22,20 +22,20 @@ const Navigation = ({selectedPage, numItemsInCart}: { selectedPage: string, numI
         <>
             <div className="relative w-full">
                 <a href="/" className="absolute left-2">
-                    <img src={"/images/logo.png"} width={"40px"} height={"40px"} />
+                    <img src={"/images/logo.png"} width={"40px"} height={"40px"}/>
                 </a>
-                <div className="flex items-center absolute right-2">
-                    <img src={"/images/cart.png"} className="" width="24px" height="20px"/>
-                    <p className="bg-amber-500 rounded-full px-2">{numItemsInCart}</p>
-                </div>
                 <div className="flex gap-4 absolute left-16">
                     {pages.map(filterValue =>
                         <a href={getPage(filterValue.path)}
-                             key={filterValue.path}
-                             className={`shrink-0 py-2 ${filterValue.pageName === selectedPage ? "font-bold" : ""}`}>
+                           key={filterValue.path}
+                           className={`shrink-0 py-2 ${filterValue.pageName === selectedPage ? "font-bold" : ""}`}>
                             {filterValue.displayName}
                         </a>
                     )}
+                </div>
+                <div className="flex items-center absolute right-2">
+                    <img src={"/images/cart.png"} className="" width="24px" height="20px"/>
+                    <p className="bg-amber-500 rounded-full px-2">{numItemsInCart}</p>
                 </div>
             </div>
         </>);
